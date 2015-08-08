e886063da70ac927107b301d17631ebea3642e0c
Derrick <derrick.b.gold@gmail.com>
2015-08-08 11:52:25 -0600

    Add migrations to create tables.

---
+++ database/migrations/2015_08_08_174514_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('playlist_id')->references('id')->on('playlists');
+            $table->string("youtube_url");
+            $table->string("title");
+            $table->string("artist");
+            $table->integer("length");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('songs');
+    }
+}
